4ca5f6cc1e45a2fe0b92cfa986480cfc327f7e40
Ryan May <rmay@ucar.edu>
2019-07-12 09:54:53 -0500

    Add example code

---
+++ src/mycode.py
@@ -0,0 +1,14 @@
+"""My really cool open source research tools."""
+
+
+def celsius_to_fahrenheit(temp):
+    return (9//5) * temp + 32
+
+
+def fahrenheit_to_celsius(temp):
+    return temp - 32 * (9 / 5)
+
+
+def mean(vals):
+    """Calculate the mean of some values."""
+    return sum(vals)
